889fd89d5b5e4fbec11771e093aee03568a5e32a
CarterT7 <129630035+CarterT7@users.noreply.github.com>
2024-08-25 21:29:50 -0500

    Added v16
    v16 – Navbar code moved from index.php to its own file, navigation.php.

---
+++ v16/pbj.php
@@ -0,0 +1,17 @@
+<div id="about" class="about-main pad-top-100 pad-bottom-100">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="wow fadeIn" data-wow-duration="1s" data-wow-delay="0.1s">
+					<h2 class="block-title">PB&amp;J</h2>
+				</div>
+			</div> <!-- end col -->
+		</div> <!-- end row -->
+
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<p>Want to learn how to make a peanut butter and jelly sandwich? Click <a href="pbj-howto.html">here</a>.</p>
+			</div>
+		</div>
+	</div> <!-- end container -->
+</div> <!-- end about -->
---
+++ v16/work.php
@@ -0,0 +1,18 @@
+<div id="about" class="about-main pad-top-100 pad-bottom-100">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="wow fadeIn" data-wow-duration="1s" data-wow-delay="0.1s">
+					<h2 class="block-title">Work</h2>
+					<p>I want to work as a software engineer when I graduate.</p>
+				</div>
+			</div> <!-- end col -->
+		</div> <!-- end row -->
+
+		<div class="row">
+			<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+				<img src="https://engineering.nyu.edu/sites/default/files/styles/content_header_default_1x/public/2018-03/program-comp-sci.jpg?h=e1d1bc8a&itok=-xmP6Hjw" alt="Coding" class="img-responsive">
+			</div>
+		</div>
+	</div> <!-- end container -->
+</div>
