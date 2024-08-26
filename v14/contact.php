3ee67adb8e36b35c8e3b3625fe91169128e815e7
CarterT7 <129630035+CarterT7@users.noreply.github.com>
2024-08-25 21:26:15 -0500

    Added v14
    v14 – Implemented server-side input validation for the contact form. When the form is submitted, all input fields are checked. If all inputs are valid, the page will refresh to display the submitted data. If any fields contain invalid inputs, the form refreshes, keeping all inputs filled so the user does not have to start over. Invalid fields are highlighted in red for easy identification.

---
+++ v14/contact.php
@@ -0,0 +1,462 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Basic -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, maximum-scale=1, initial-scale=1, user-scalable=0">
+
+    <!-- Site Metas -->
+    <title>Contact</title>
+
+    <!-- Site Icons -->
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!-- Site CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    <!-- color -->
+    <link id="changeable-colors" rel="stylesheet" href="assets/css/colors/orange.css" />
+
+    <!-- Modernizer -->
+    <script src="assets/js/modernizer.js"></script>
+</head>
+
+<body>
+	<div id="loader">
+	    <div id="status"></div>
+	</div>
+	<div id="site-header">
+  	<header id="header" class="header-block-top fixed-menu">
+    	<div class="container">
+    		<div class="row">
+        		<div class="main-menu">
+					<!-- navbar -->
+					<nav class="navbar navbar-default" id="mainNav">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<div class="logo">
+								<a class="navbar-brand js-scroll-trigger logo-header" href="#">
+									<h3 class="navbar-collapse collapse website-title">Carter's Website</h3>
+								</a>
+							</div>
+						</div>
+						
+						<div id="navbar" class="navbar-collapse collapse">
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="index.html">Home</a></li>
+								<li><a href="school.html">School Info</a></li>
+								<li><a href="hobbies.html">Hobbies &amp; Interests</a></li>
+								<li><a href="work.html">Work</a></li>
+								<li><a href="pbj.html">PB&amp;J</a></li>
+								<li class="active"><a href="contact.php">Contact</a></li>
+							</ul>
+						</div> <!-- end nav-collapse -->
+					</nav> <!-- end navbar -->
+				</div> <!-- end main menu -->
+			</div> <!-- end row -->
+		</div> <!-- end container-fluid -->
+	</header> <!-- end header -->
+	</div> <!-- end site-header -->
+	
+	<?php
+	// use a session to allow us to retain the data inside the POST superglobals, which get cleared upon redirection (PHP's header() function). More on this later in the code.
+	session_start();
+	
+	if (!isset($_POST['submit'])) 
+	{
+		echo '<form id="contactForm" method="post" action="">';
+		echo   '<div id="about" class="about-main pad-top-100 pad-bottom-100">';
+		echo     '<div class="container">';
+		echo       '<div class="row">';
+		echo         '<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">';
+		echo           '<div class="wow fadeIn" data-wow-duration="1s" data-wow-delay="0.1s">';
+		echo             '<h2 class="block-title">Contact</h2>';
+		echo           '</div>';
+		echo         '</div>'; // end col
+		echo       '</div>'; // end row
+
+		/* Note: 'id' attribute is used to identify an HTML element via CSS or JavaScript. Each 'id' must be unique.
+				 'name' attribute is used by the server as a variable name to reference data submitted in a form element. 'name' may not be unique, for example, in the case of radio elements. */
+				
+		// For this app: 'id' attribute is for JavaScript; 'name' attribute is for PHP.
+		echo '<div class="col-lg-4">';
+
+		$errMsg = $_GET['errMsg'];
+		
+		// First name error checking
+		if (isset($errMsg) && strstr($errMsg, "firstName")) {
+			
+			if (strstr($errMsg, "firstNameNull"))
+				$msg = 'First name cannot be blank';
+			else if (strstr($errMsg, "firstNameLength"))
+				$msg = 'First name must be between 2 and 120 characters';
+			else if (strstr($errMsg, "firstNameNoAlphaChars"))
+				$msg = 'First name must contain at least one alphabetic character';
+			else if (strstr($errMsg, "firstNameInvalidChar"))
+				$msg = 'First name can only contain letters, hyphens, and apostrophes';
+			
+			echo '<div class="form-box has-error" id="fnBox">';
+			echo 	'<label class="control-label" for="firstName">Enter first name:</label>';
+			echo 	'<input type="text" class="form-control" id="firstName" name="firstName" value="'. $_SESSION['firstName'] .'"/>';
+			echo 	'<div id="fnFeedback" class="help-block">'.$msg.'</div>';
+			echo '</div>';
+		}
+		else {
+			// if session data exists for firstName, fill in the data and make box green
+			if (isset($_SESSION['firstName'])) {
+				echo '<div class="form-box has-success" id="fnBox">';
+				echo   '<label class="control-label" for="firstName">Enter first name:</label>';
+				echo 	 '<input type="text" class="form-control" id="firstName" name="firstName" value="'. $_SESSION['firstName'] .'"/>';
+			}
+			else {
+				echo '<div class="form-box" id="fnBox">';
+				echo   '<label class="control-label" for="firstName">Enter first name:</label>';
+				echo 	 '<input type="text" class="form-control" id="firstName" name="firstName"/>';
+			}
+			echo   '<div id="fnFeedback"></div>';
+			echo '</div>';
+		}
+
+				
+		// last name error checking
+		if (isset($errMsg) && strstr($errMsg, "lastName")) {
+			if (strstr($errMsg, "lastNameNull"))
+				$msg = 'Last name cannot be blank';
+			else if (strstr($errMsg, "lastNameLength"))
+				$msg = 'Last name must be between 2 and 120 characters';
+			else if (strstr($errMsg, "lastNameNoAlphaChars"))
+				$msg = 'Last name must contain at least one alphabetic character';
+			else if (strstr($errMsg, "lastNameInvalidChar"))
+				$msg = 'Last name can only contain letters, hyphens, and apostrophes';
+			
+			echo '<div class="form-box has-error" id="lnBox">';
+			echo 	'<label class="control-label" for="lastName">Enter last name:</label>';
+			echo 	'<input type="text" class="form-control" id="lastName" name="lastName" value="'. $_SESSION['lastName'] .'"/>';
+			echo 	'<div id="lnFeedback" class="help-block">'.$msg.'</div>';
+			echo '</div>';
+		}
+		else {
+			if (isset($_SESSION['lastName'])) {
+				echo '<div class="form-box has-success" id="lnBox">';
+				echo 	'<label class="control-label" for="lastName">Enter last name:</label>';
+				echo 	'<input type="text" class="form-control" id="lastName" name="lastName" value="' . $_SESSION['lastName'] . '"/>';
+			}
+			else {
+				echo '<div class="form-box" id="lnBox">';
+				echo 	'<label class="control-label" for="lastName">Enter last name:</label>';
+				echo 	'<input type="text" class="form-control" id="lastName" name="lastName"/>';
+			}
+			echo 	'<div id="lnFeedback"></div>';
+			echo '</div>';
+		}
+
+		
+		// email error checking
+		if (isset($errMsg) && strstr($errMsg, "email")) {
+			if (strstr($errMsg, "emailNull"))
+				$msg = 'Email cannot be blank';
+			else if (strstr($errMsg, "emailInvalid"))
+				$msg = 'Invalid email';
+			
+			echo '<div class="form-box has-error" id="emBox">';
+			echo 	'<label class="control-label" for="email">Enter email:</label>';
+			echo 	'<input type="text" class="form-control" id="email" name="email" value="'. $_SESSION['email'] .'"/>';
+			echo 	'<div id="emFeedback" class="help-block">'.$msg.'</div>';
+			echo '</div>';
+		}
+		else {
+			if (isset($_SESSION['email'])) {
+				echo '<div class="form-box has-success" id="emBox">';
+				echo 	'<label class="control-label" for="email">Enter email:</label>';
+				echo 	'<input type="text" class="form-control" id="email" name="email" value="'. $_SESSION['email'] .'"/>';
+			}
+			else {
+				echo '<div class="form-box" id="emBox">';
+				echo 	'<label class="control-label" for="email">Enter email:</label>';
+				echo 	'<input type="text" class="form-control" id="email" name="email"/>';
+			}
+			echo 	'<div id="emFeedback"></div>';
+			echo '</div>';
+		}
+		
+		
+		// phone number error checking
+		if (isset($errMsg) && strstr($errMsg, "phoneNumber")) {
+			if (strstr($errMsg, "phoneNumberNull"))
+				$msg = 'Phone number cannot be blank';
+			else if (strstr($errMsg, "phoneNumberNonNumeric"))
+				$msg = 'Phone number must contain only numbers';
+			else if (strstr($errMsg, "phoneNumberMismatch"))
+				$msg = 'Phone number must be exactly 10 digits';
+			
+			echo '<div class="form-box has-error" id="phBox">';
+			echo 	'<label class="control-label" for="phoneNumber">Enter phone number:</label>';
+			echo 	'<input type="text" class="form-control" id="phoneNumber" name="phoneNumber" value="'. $_SESSION['phoneNumber'] .'"/>';
+			echo 	'<div id="phFeedback" class="help-block">'.$msg.'</div>';
+			echo '</div>';
+		}
+		else {
+			if (isset($_SESSION['phoneNumber'])) {
+				echo '<div class="form-box has-success" id="phBox">';
+				echo 	'<label class="control-label" for="phoneNumber">Enter phone number:</label>';
+				echo 	'<input type="text" class="form-control" id="phoneNumber" name="phoneNumber" value="' . $_SESSION['phoneNumber'] . '"/>';
+			}
+			else {
+				echo '<div class="form-box" id="phBox">';
+				echo 	'<label class="control-label" for="phoneNumber">Enter phone number:</label>';
+				echo 	'<input type="text" class="form-control" id="phoneNumber" name="phoneNumber"/>';
+			}
+			echo 	'<div id="phFeedback"></div>';
+			echo '</div>';
+		}
+		
+		
+		// username error checking
+		if (isset($errMsg) && strstr($errMsg, "username")) {
+			if (strstr($errMsg, "usernameNull"))
+				$msg = 'Username cannot be blank';
+			else if (strstr($errMsg, "usernameLength"))
+				$msg = 'Username must be between 6 and 150 characters';
+			
+			echo '<div class="form-box has-error" id="unBox">';
+			echo 	'<label class="control-label" for="username">Create a username:</label>';
+			echo 	'<input type="text" class="form-control" id="username" name="username" value="'. $_SESSION['username'] .'"/>';
+			echo 	'<div id="unFeedback" class="help-block">'.$msg.'</div>';
+			echo '</div>';
+		}
+		else {
+			if (isset($_SESSION['username'])) {
+				echo '<div class="form-box has-success" id="unBox">';
+				echo 	'<label class="control-label" for="username">Create a username:</label>';
+				echo 	'<input type="text" class="form-control" id="username" name="username" value="' . $_SESSION['username'] . '"/>';
+			}
+			else {
+				echo '<div class="form-box" id="unBox">';
+				echo 	'<label class="control-label" for="username">Create a username:</label>';
+				echo 	'<input type="text" class="form-control" id="username" name="username"/>';
+			}
+			echo 	'<div id="unFeedback"></div>';
+			echo '</div>';
+		}
+		
+		// password error checking
+		if (isset($errMsg) && strstr($errMsg, "password")) {
+			if (strstr($errMsg, "passwordNull"))
+				$msg = 'Password cannot be blank';
+			else if (strstr($errMsg, "passwordLength"))
+				$msg = 'Password must be between 6 and 250 characters';
+			
+			echo '<div class="form-box has-error" id="pwBox">';
+			echo 	'<label class="control-label" for="password">Create a password:</label>';
+			echo 	'<input type="password" class="form-control" id="password" name="password" value="'. $_SESSION['password'] .'"/>';
+			echo 	'<div id="pwFeedback" class="help-block">'.$msg.'</div>';
+			echo '</div>';
+		}
+		else {
+			if (isset($_SESSION['password'])) {
+				echo '<div class="form-box has-success" id="pwBox">';
+				echo 	'<label class="control-label" for="password">Create a password:</label>';
+				echo 	'<input type="password" class="form-control" id="password" name="password" value="' . $_SESSION['password'] . '"/>';
+			}
+			else {
+				echo '<div class="form-box" id="pwBox">';
+				echo 	'<label class="control-label" for="password">Create a password:</label>';
+				echo 	'<input type="password" class="form-control" id="password" name="password"/>';
+			}
+			echo 	'<div id="pwFeedback"></div>';
+			echo '</div>';
+		}
+		
+		// comments error checking
+		if (isset($errMsg) && strstr($errMsg, "comments")) {
+			if (strstr($errMsg, "commentsNull"))
+				$msg = 'Comments section cannot be blank';
+			else if (strstr($errMsg, "commentsLength"))
+				$msg = 'Comments section must not exceed 2500 characters';
+			
+			echo '<div class="form-box has-error" id="comBox">';
+			echo 	'<label class="control-label" for="comments">Enter your comments here:</label>';
+			echo 	'<textarea type="text" class="form-control" id="comments" name="comments" rows="5">'.$_SESSION['comments']. '</textarea>';
+			echo 	'<div id="comFeedback" class="help-block">'.$msg.'</div>';
+			echo '</div>';
+		}
+		else {
+			if (isset($_SESSION['comments'])) {
+				echo '<div class="form-box has-success" id="comBox">';
+				echo 	'<label class="control-label" for="comments">Enter your comments here:</label>';
+				echo 	'<textarea type="text" class="form-control" id="comments" name="comments" rows="5">'.$_SESSION['comments']. '</textarea>';
+			}
+			else {
+				echo '<div class="form-box" id="comBox">';
+				echo 	'<label class="control-label" for="comments">Enter your comments here:</label>';
+				echo 	'<textarea type="text" class="form-control" id="comments" name="comments" rows="5"></textarea>';
+			}
+			echo 	'<div id="comFeedback"></div>';
+			echo '</div>';
+
+		}
+
+		// Submit button
+		echo '<div class="reserve-book-btn">';
+		echo	'<button class="hvr-underline-from-center" type="submit" name="submit">Send</button>';
+		echo '</div>';
+
+		echo '</div>'; // end col
+		echo '</div>'; // end container
+		echo '</div>'; // end "about" div
+		echo '</form>';
+
+	}
+
+	else {
+
+		// even though we have JavaScript to notify user of incorrect fields (and even if we had it set up to reject the "Submit button for incorrect inputs using JS)", we still need to validate the input data on server-side, because JavaScript can be disabled on a web browser.
+		
+		// clear out previous errors
+		$errors = "";
+		
+		// clear any previous session data (?? doesn't work)
+		unset($_SESSION['firstName']);
+		unset($_SESSION['lastName']);
+		unset($_SESSION['email']);
+		unset($_SESSION['phoneNumber']);
+		unset($_SESSION['username']);
+		unset($_SESSION['password']);
+		unset($_SESSION['comments']);
+		
+		// begin processing newly submitted data
+		
+		$firstName = $_POST['firstName'];
+		$lastName = $_POST['lastName'];
+		$email = $_POST['email'];
+		$phoneNumber = $_POST['phoneNumber'];
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$comments = $_POST['comments'];
+
+
+		// check first name
+		if ($firstName == NULL)
+			$errors .= "firstNameNull";
+		else if (strlen($firstName) < 2 || strlen($firstName) > 120)
+			$errors .= "firstNameLength";
+		else if (preg_match("/[a-zA-Z]/", $firstName) !== 1)
+			$errors .= "firstNameNoAlphaChars";
+		else if (preg_match("/^[a-zA-Z-']{2,120}$/", $firstName) !== 1)
+			$errors .= "firstNameInvalidChar";
+		
+		$_SESSION['firstName'] = $firstName;
+		
+		
+		// check last name
+		if ($lastName == NULL)
+			$errors.="lastNameNull";
+		else if (strlen($lastName) < 2 || strlen($lastName) > 120)
+			$errors .= "lastNameLength";
+		else if (preg_match("/[a-zA-Z]/", $lastName) !== 1)
+			$errors .= "lastNameNoAlphaChars";
+		else if (preg_match("/^[a-zA-Z-']{2,120}$/", $lastName) !== 1)
+			$errors .= "lastNameInvalidChar";
+		
+		$_SESSION['lastName'] = $lastName;
+		
+				
+		// check email
+		if ($email == NULL)
+			$errors.="emailNull";
+		else if (preg_match('/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/', $email) !== 1)
+			$errors .= "emailInvalid";
+		
+		$_SESSION['email'] = $email;
+		
+		
+		// check phone number
+		if ($phoneNumber == NULL)
+			$errors.="phoneNumberNull";
+		else if (!ctype_digit($phoneNumber))
+			$errors .= "phoneNumberNonNumeric";
+		else if (strlen($phoneNumber) != 10)
+			$errors .= "phoneNumberMismatch";
+		
+		$_SESSION['phoneNumber'] = $phoneNumber;
+		
+		
+		// check username
+		if ($username == NULL)
+			$errors.="usernameNull";
+		else if (strlen($username) < 6 || strlen($username) > 150)
+			$errors .= "usernameLength";
+		
+		$_SESSION['username'] = $username;
+		
+		
+		// check password
+		if ($password == NULL) {
+			$errors .= "passwordNull";
+		}
+		else if (strlen($password) < 6 || strlen($password) > 250)
+			$errors .= "passwordLength";
+		
+		$_SESSION['password'] = $password;
+		
+		
+		// check comments
+		if ($comments == NULL) {
+			$errors .= "commentsNull";
+		}
+		else if (strlen($comments) > 2500)
+			$errors .= "commentsLength";
+		
+		$_SESSION['comments'] = $comments;
+		
+		
+		// if there are errors, redirect to contact.php with appropriate error message
+		if ($errors != NULL)
+			header("Location: contact.php?errMsg=$errors");
+
+		// if we reach this point, there are no errors, so print the contact form data
+		echo '<div id="about" class="about-main pad-top-100 pad-bottom-100">';
+		echo   '<div class="container">';
+		echo 	 '<div class="row">';
+		echo 	   '<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">';
+		echo 		 '<div class="wow fadeIn" data-wow-duration="1s" data-wow-delay="0.1s">';
+		echo 		   '<h2 class="block-title">Contact Form Data</h2>';
+		echo 		   '<p><b>First Name:</b> ' . $_POST['firstName'] . '</p>';
+		echo 		   '<p><b>Last Name:</b> ' . $_POST['lastName'] . '</p>';
+		echo 		   '<p><b>Email:</b> ' . $_POST['email'] . '</p>';
+		echo 		   '<p><b>Phone Number:</b> ' . $_POST['phoneNumber'] . '</p>';
+		echo 		   '<p><b>Username:</b> ' . $_POST['username'] . '</p>';
+		echo 		   '<p><b>Password:</b> ' . $_POST['password'] . '</p>';
+		echo 		   '<p><b>Comments:</b> ' . $_POST['comments'] . '</p>';
+		echo		 '</div>'; // end "wow fadeIn" div
+		echo	   '</div>'; // end col
+		echo 	 '</div>'; // end row
+		echo   '</div>'; // end container
+		echo '</div>'; // end "about" div
+	}
+	?>
+	
+	<a href="#" class="scrollup" style="display: none;">Scroll</a>
+	
+	<!-- ALL JS FILES -->
+	<script src="assets/js/all.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/event-listener.js"></script>
+	<!-- ALL PLUGINS -->
+	<script src="assets/js/custom.js"></script>
+	</body>
+	
+</html>
